6bd23b9c97d17a9cf5439c5b77b2b138f9acba42
Daniel Malcolm <daniel.malcolm@lego.com>
2023-01-14 19:36:11 +0000

    edit error type to any
    discovered try catch forces error object on catch to be any / unknown

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { Logger as AWSLogger } from "@aws-lambda-powertools/logger";
 interface log {
   message: string;
   level: "info" | "warn" | "error" | "debug";
-  error?: Error;
+  error?: any;
   data?: any;
 }
 
@@ -42,7 +42,7 @@ export class Logger {
     this.debugLogs.push({ message, level: "debug", data });
   }
 
-  error(message: string, error: Error, data?: any) {
+  error(message: string, error: any, data?: any) {
     this.log({ message, level: "error", error, data });
     this.printDebugLogs();
     this.debugLogs = [];
