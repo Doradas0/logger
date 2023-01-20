1826d3a81e36c899ab130ab08350419d720696ae
Daniel Malcolm <daniel.malcolm@lego.com>
2023-01-20 16:12:26 +0000

    add examples to readme

--- src/logger.test.ts
+++ src/logger.test.ts
@@ -10,8 +10,10 @@ describe("Logger", () => {
     logger.warn("Invalid Request", { requestId: "123", missingParams: ["foo"] });
   });
   it("should log debug then .5 seconds later log error", async () => {
-    logger.debug("test", { foo: "bar" });
+    const event = { foo: "bar" };
+    logger.debug("test", event);
     await new Promise((resolve) => setTimeout(resolve, 500));
-    logger.error("test", new Error("test"), { foo: "baz" });
+    const error = new Error("test error");
+    logger.error("test", error);
   });
 });
