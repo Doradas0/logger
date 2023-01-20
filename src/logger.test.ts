98ecc8d3e4f6508586e97054b147ceba14ee3eb5
Daniel Malcolm <daniel.malcolm@lego.com>
2023-01-20 15:22:04 +0000

    update test to show all logger functions

--- src/logger.test.ts
+++ src/logger.test.ts
@@ -3,9 +3,15 @@ import { Logger } from "./logger";
 const logger = new Logger("test-service");
 
 describe("Logger", () => {
-  it("should log debug then 2 seconds later log error", async () => {
+  it("should log info", () => {
+    logger.info("Order Updated");
+  });
+  it("should log warn", () => {
+    logger.warn("Invalid Request", { requestId: "123", missingParams: ["foo"] });
+  });
+  it("should log debug then .5 seconds later log error", async () => {
     logger.debug("test", { foo: "bar" });
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 500));
     logger.error("test", new Error("test"), { foo: "baz" });
   });
 });
